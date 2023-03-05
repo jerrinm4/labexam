701b762b64381a590df67765740dca99dbb09f92
jerrinm4 <jerrinm4@gmail.com>
2023-03-05 18:21:44 +0530

    first commit

--- new.js
+++ new.js
@@ -1,4 +1,4 @@
-$.getJSON('./programs', data => {
+$.getJSON('/programs', data => {
     console.log(data);
     data.forEach(element => {
        $("#codeslist").append("<li><a href='/code.html#"+element+"' target='_blank'>"+element+"</a></li>");
