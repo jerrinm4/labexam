c76776267e3a0ba535020b45867d71febc420cae
jerrinm4 <jerrinm4@gmail.com>
2023-03-05 19:11:46 +0530

    fcommit

--- new.js
+++ new.js
@@ -1,7 +1,26 @@
-$.getJSON('/programs', data => {
-    console.log(data);
-    data.forEach(element => {
-       $("#codeslist").append("<li><a href='/code.html#"+element+"' target='_blank'>"+element+"</a></li>");
+$.getJSON('https://api.github.com/repos/jerrinm4/labexam/git/trees/main', data => {
+    console.log(data['tree']);
+    data['tree'].forEach(element => {
+      if(element['path']=="programs"){
+         console.log(element["url"]);
+         $.getJSON(element["url"], data => {
+    data['tree'].forEach(element1 => {
+      console.log(element1['path'])
+       $("#codeslist").append("<li><a href='/code.html#"+element1['path']+"' target='_blank'>"+element1['path']+"</a></li>");
 
     });
     });
+      }
+
+    });
+    });
+
+
+
+   //  $.getJSON('/programs', data => {
+   //    console.log(data);
+   //    data.forEach(element => {
+   //       $("#codeslist").append("<li><a href='/code.html#"+element+"' target='_blank'>"+element+"</a></li>");
+  
+   //    });
+   //    });
